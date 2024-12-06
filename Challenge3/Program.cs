486b35711022ad619fb1277309dd4f5508f29c0f
colinc1991 <34336297+colinc1991@users.noreply.github.com>
2024-12-06 15:00:54 +0000

    Challenge 3

---
+++ Challenge3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.RegularExpressions;
+
+string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\..\\reports.txt");
+string content = File.ReadAllText(path);
+var reportsList = content.Split("\r\n");
+var numberOfSafeReports = 0;
+
+foreach (var report in reportsList)
+{
+    var splitReport = report.Split(" ").Select(x => Convert.ToInt32(x)).ToList();
+
+    var ascendingReport = splitReport.Select(x => x).OrderBy(x => x).ToList();
+    var descendingReport = splitReport.Select(x => x).OrderByDescending(x => x).ToList();
+    
+    if (!descendingReport.SequenceEqual(splitReport) && !ascendingReport.SequenceEqual(splitReport))
+    {
+        continue;
+    }
+
+    for (int i = 0; i < splitReport.Count; i++)
+    {
+        // last element, we're done
+        if (i == splitReport.Count - 1)
+        {
+            numberOfSafeReports++;
+            continue;
+        }
+
+        var firstReport = splitReport[i];
+        var secondReport = splitReport[i+1];
+
+        if (firstReport == secondReport)
+        {
+            break;
+        }
+
+        var difference = 0;
+        
+        if (firstReport > secondReport)
+        {
+            difference = firstReport - secondReport;
+        }
+        else
+        {
+            difference = secondReport - firstReport;
+        }
+
+        // too much of a difference, unsafe!
+        if (difference > 3)
+        {
+            break;
+        }
+    }
+}
+
+Console.WriteLine(numberOfSafeReports);
